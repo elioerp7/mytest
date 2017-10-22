8372bba5d2386d6712ba86b9de0dbee24ccbe44c
Elio Rosabal <elio_erp@hotmail.com>
2017-10-22 17:46:05 -0400

    new

---
+++ mytest/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mytest.Models
+{
+    public class Book
+    {
+
+        [Key]
+        [MaxLength(17)]
+        public string ISBN { get; set; }
+
+        public string Title { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string Author { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Publisher { get; set; }
+
+        [MinLength(0)]
+        public int Quantity { get; set; }
+
+        [MaxLength(255)]
+        public string Image { get; set; }
+
+        public int IsFeatured { get; set; }
+
+    }
+}
