1b1c24da9e7817be74326f3183fe8d8d10f794ea
Andreina Rojas <aroja108@fiu.edu>
2017-10-27 19:42:39 -0400

    home

--- mytest/Controllers/HomeController.cs
+++ mytest/Controllers/HomeController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using mytest.Data;
 
 namespace mytest.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_context.Books.ToList());
         }
+
+
 
         public IActionResult About()
         {
@@ -30,6 +40,43 @@ namespace mytest.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+        public IActionResult ShowBook(string field)
+        {
+            if (field == null)
+                return View();
+            else
+            {
+                ViewBag.BookISBN = field;
+                var comments = _context.Comments.Where(d => d.BookISBN.Equals(field)).ToList();
+                ViewBag.Comments = comments;
+
+                var ratings = _context.Comments.Where(d => d.BookISBN.Equals(field)).ToList();
+
+                if (ratings.Count() > 0)
+                {
+                    var ratingSum = ratings.Sum(d => d.Rating.Value);
+                    ViewBag.RatingSum = ratingSum;
+                    var ratingCount = ratings.Count();
+                    ViewBag.RatingCount = ratingCount;
+                }
+                else
+                {
+                    ViewBag.RatingSum = 0;
+                    ViewBag.RatingCount = 0;
+                }
+
+                return View(_context.Books.Where(x => x.ISBN.Equals(field) || field == null).ToList());
+            }
+        }
+        public IActionResult ShowAuthor(string field)
+        {
+            if (field == null)
+                return View();
+            else
+            {
+                return View(_context.Books.Where(x => x.Author.ToLower().Equals(field.ToLower()) || field == null).ToList());
+            }
         }
     }
 }
