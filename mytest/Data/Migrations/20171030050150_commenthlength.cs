0bba58833076f71a54c7eb132871b932ac2e4b05
Elio Rosabal <elio_erp@hotmail.com>
2017-10-30 01:02:14 -0400

    migration comment length

---
+++ mytest/Data/Migrations/20171030050150_commenthlength.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mytest.Data.Migrations
+{
+    public partial class commenthlength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comments",
+                table: "Comments",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Comments",
+                table: "Comments",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 150,
+                oldNullable: true);
+        }
+    }
+}
